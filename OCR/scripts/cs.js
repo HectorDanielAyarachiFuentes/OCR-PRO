478775c657790e103ce771f24df05e4305f46639
Ayarachi Fuentes Hector Daniel <mp4o@hotmail.com>
2026-08-01 12:26:10 -0300

    feat: implement OCR dialog UI and core content script logic for enhanced text recognition

--- OCR/scripts/cs.js
+++ OCR/scripts/cs.js
@@ -2096,9 +2096,9 @@ if (typeof browser === "undefined") {
 			$('#popup_translate_button img').attr('src', browser.runtime.getURL('OCR/images/translate.png'));
 
 			if (OPTIONS.status && OPTIONS.status.toLowerCase() === 'free plan') {
-				$('.ocrext-title-name').html(appName + ' <small style="font-size: 0.6em; opacity: 0.8; font-weight: normal; margin-left: 4px;">v1.0.0</small>');
+				$('.ocrext-title-name').html(appName + ' <small style="font-size: 0.6em; opacity: 0.8; font-weight: normal; margin-left: 4px;">v1.0.1</small>');
 			} else {
-				$('.ocrext-title-name').html(appName + ' <small style="font-size: 0.6em; opacity: 0.8; font-weight: normal; margin-left: 4px;">v1.0.0</small>');
+				$('.ocrext-title-name').html(appName + ' <small style="font-size: 0.6em; opacity: 0.8; font-weight: normal; margin-left: 4px;">v1.0.1</small>');
 			}
 			// set paragraph font
 			_setLanguageOnUI();
@@ -2218,9 +2218,9 @@ if (typeof browser === "undefined") {
 				.find('.ocrext-wrapper')
 				.hide();
 			if (OPTIONS.status && OPTIONS.status.toLowerCase() === 'free plan') {
-				$('.ocrext-title-name').html(appName + ' <small style="font-size: 0.6em; opacity: 0.8; font-weight: normal; margin-left: 4px;">v1.0.0</small>');
+				$('.ocrext-title-name').html(appName + ' <small style="font-size: 0.6em; opacity: 0.8; font-weight: normal; margin-left: 4px;">v1.0.1</small>');
 			} else {
-				$('.ocrext-title-name').html(appName + ' <small style="font-size: 0.6em; opacity: 0.8; font-weight: normal; margin-left: 4px;">v1.0.0</small>');
+				$('.ocrext-title-name').html(appName + ' <small style="font-size: 0.6em; opacity: 0.8; font-weight: normal; margin-left: 4px;">v1.0.1</small>');
 			}
 			OCRTranslator.reset();
 			// show mask
